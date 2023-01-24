bc5271007a6388cecd60aeda26cce9356b78ccf9
muhajir29 <imammuhajir92@gmail.com>
2023-01-24 22:35:39 +0000

    set up terraform success with all of setting

--- week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
+++ week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
@@ -10,7 +10,7 @@ variable "project" {
 
 variable "region" {
   description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
-  default = "asia-southeast1-b"
+  default = "asia-southeast1"
   type = string
 }
 
